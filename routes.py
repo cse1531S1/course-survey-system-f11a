fb3f1f838cc3f6dd81a68d1f9df58da064284b3c
AndrewRCCarmichael <z5060054@student.unsw.edu.au>
2017-08-17 13:56:22 +1000

    Basic website functionality

---
+++ routes.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, render_template, request, url_for
+from server import app, user_input, question_list
+import csv
+
+#https://www.w3schools.com/w3css/w3css_templates.asp
+
+#LOGIN PAGE
+@app.route('/', methods=["GET","POST"])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			return redirect(url_for('dashboard'))
+	return render_template('login.html', error=error)
+
+#ADMIN DASHBOARD
+@app.route('/Dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+	
+
+@app.route('/NewQuestions',methods=["GET","POST"])
+def newquestions():
+	if request.method == 'POST':
+		question = request.form['q']
+		with open('questionList.csv','a') as csv_out:
+			writer = csv.writer(csv_out)
+			writer.writerow([question])
+	return render_template('newquestions.html')
+
+@app.route('/NewSurvey')
+def newsurvey():
+	return render_template('newsurvey.html')
+
+@app.route('/QuestionList')
+def questionlist():
+	with open('questionList.csv','r') as csv_in:
+		reader = csv.reader(csv_in)
+		question_list = list(reader)
+	return render_template('questions.html', questions = question_list)
