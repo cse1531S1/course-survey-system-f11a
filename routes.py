9c6fe5eecb10714e2872616814a7e8be3145ed58
Alex Hirsch <alex_hirsch@hotmail.com>
2017-08-26 11:28:56 +1000

    Printing semesters

--- routes.py
+++ routes.py
@@ -4,6 +4,40 @@ import csv
 
 #https://www.w3schools.com/w3css/w3css_templates.asp
 
+def inList(list_current, to_find):
+   # Takes in a list and a item
+   # Determines if the item is in the list
+    found = False
+    for item in list_current:
+        if(item == to_find):
+            found = True
+            return found #early exit
+    return found
+         
+         
+def get_courses():
+    courses = []
+    found = True
+    while(found): #while a unique course has not been added
+        found = False
+        with open('courses.csv','r') as csv_in: #open the csv
+            reader = csv.reader(csv_in)
+            next_min = "zzzzz"
+            for row in reader: #for each row in the csv
+                if(row != []):
+                    #print(row)
+                    #print("and 1 is")
+                    #print(row[1])
+                    if(row[1] < next_min): #if the current semester is less than the minimum
+                        if(inList(courses, row[1]) == False): #check not already in 
+                            next_min = row[1]
+                            found = True
+            print("Newest course is " + next_min)
+            if(found):
+                courses.append(next_min)
+        #found = True
+    return courses
+
 #LOGIN PAGE
 @app.route('/', methods=["GET","POST"])
 def login():
@@ -23,18 +57,22 @@ def dashboard():
 #NEW QUESTIONS PAGE
 @app.route('/NewQuestions',methods=["GET","POST"])
 def newquestions():
-	if request.method == 'POST':
-		question = request.form['q']
-		with open('questionList.csv','a') as csv_out:
-			writer = csv.writer(csv_out)
-			writer.writerow([question])
-	return render_template('newquestions.html')
+    if request.method == 'POST':
+        question = request.form['q']
+        with open('questionList.csv','a') as csv_out:
+            writer = csv.writer(csv_out)
+            writer.writerow([question])
+    return render_template('newquestions.html')
 
 #NEW SURVEY PAGE
 @app.route('/NewSurvey')
 def newsurvey():
-	# TODO
-	return render_template('newsurvey.html')
+    courses_list = []
+    if True:
+        # open course list csv and add to list of lists
+	    courses_list = get_courses()
+	    #pass though list of lists
+    return render_template('newsurvey.html', courses = courses_list)
 
 #QUESTION LIST PAGE
 @app.route('/QuestionList')
@@ -44,3 +82,13 @@ def questionlist():
 		question_list = list(reader)
 		stringVersion = "<br/>".join(item[0] for item in question_list)
 	return render_template('questions.html', questions = stringVersion)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
