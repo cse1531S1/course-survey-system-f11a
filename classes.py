daae4a6c61a58a9b88953361aecc0dc7ef22a962
AndrewRCCarmichael <z5060054@student.unsw.edu.au>
2017-09-25 10:29:37 +1000

    Question/Survey database functionality there; not yet tested

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
+#CHANGE SURVEY SO THAT IT ONLY INCLUDES A LIST OF QUESTION IDS, AND NOT A QUESTION POOL
+
 class User():
-	def __init__(self, newID, permLevel, DBName):
-		self._dbName = DBName
+	def __init__(self, newID, permLevel, dbName):
+		self._dbName = dbName
 		self._id = newID
 		self._permLevel = permLevel
 		self._courses = []
@@ -55,6 +57,7 @@ class SurveyPool(Object):
 		self._dbName = dbName
 		self._surveys = []
 		self._idCounter = 0
+
 	def getSurvey(surveyID):
 		retVal = None
 		for survey in self._surveys:
@@ -88,6 +91,7 @@ class QuestionPool(Object):
 	def __init__(self, dbName):
 		self._dbName = dbName
 		self._questions = []
+		self._questionCounter = 0
 
 	def getQuestion(questionID):
 		retVal = None
@@ -101,14 +105,40 @@ class QuestionPool(Object):
 			if(question.getQuestionID() == questionID):
 				self._questions.remove(question)
 
+	def addQuestion(self, qString, answerType, isMandatory):
+		q = Question(self._questionCounter, qString, answerType, isMandatory)
+		self._questions.append(q)
+		self._questionCounter++
+		writer = SQLWriter()
+		query = "INSERT INTO Questions (QID, QString, AnswerType, IsMandatory) VALUES ('%s', '%s', '%s', '%s')" % (self._questionCounter, qString, answerType, isMandatory)
+		writer.dbinsert(query, self.dbName)	
+
 	def generatePool(self):
-		#PENDING DATABASE CONFIG
+		writer = SQLWriter()
+		i = 0
+		while True:
+			query = "SELECT * FROM Questions WHERE rowid = %s" % str(i) 
+			retVal = writer.dbselect(query, self.dbName)
+			if retVal == []:
+				break
+			else:
+				newq = Question(retVal[0], retVal[1], retVal[2], retVal[3])
+				self._questions.append(newq)
+				if int(retVal[0]) > self._questionCounter:
+					self._questionCounter = int(retVal[0])
+			i++
 
 	def storePool(self):
-		#PENDING DATABASE CONFIG
+		writer = SQLWriter()
+		for q in self._questions:
+			query = "INSERT INTO Questions (QID, QString, AnswerType, IsMandatory) VALUES ('%s', '%s', '%s', '%s')" % (q.getQuestionID(), q.getQuestionString(), q.getAnswerType(), q.getIsMandatory())
+			writer.dbinsert(query, self.dbName)
 
 	def clearPool(self):
-		#PENDING DATABASE CONFIG
+		query = "DELETE * FROM Questions"
+		SQLWriter.dbselect(query)
+		self._questions = []
+		self._questionCounter = 0
 
 	def getQuestionList():
 		return self._questions
@@ -146,50 +176,56 @@ class Survey(Object):
 	def __init__(self, coursename, uniqueID):
 		self._coursename = coursename
 		self._uniqueID = uniqueID
-		self._questionPool = QuestionPool("InitData.db")
+		self._questionList = [] #The question pool is merely a list of unique numbers
 		self._responsePool = ResponsePool("")#TBD
 	
 	def getCourseName(self):
 		return self._coursename
 
+	#Given a question, add the question ID to the pool
 	def addQuestion(self, q):
-		self._questionPool.addQuestion(q)
+		self._questionList.append(q.getQuestionID())
 	
+	#Given a list of question IDs, set the list of QIDs to those questions
 	def setQuestions(self, newQuestions):
-		self._questionPool.setQuestions(newQuestions)
+		self._questionList = newQuestions
 
+	#Return the question ID list
 	def getQuestions(self):
-		return self._questionPool.getQuestions();
+		return self._questionList;
 
+	#Add a response
 	def addResponse(self, newResponse):
 		self._responsePool.addResponse(newResponse)
 
+	#Set the responsepool list of objects to the newly given list of objects
 	def setResponses(self, newResponses):
 		self._responsePool.setResponses(newResponses)
 
+	#Returns the current list of response objects
 	def getResponses(self):
 		return self._responsePool.getResponses()
 
-	def generateSurvey(self):
-		#TBD
-
-	def storeSurvey(self):
-		#TBD
-
+	def generateQuestions(self):
+		writer = SQLWriter()
+		i = 0
+		while True:
+			query = "SELECT * FROM Questions WHERE rowid = %s" % str(i) 
+			retVal = writer.dbselect(query, self.dbName)
+			if retVal == []:
+				break
+			else:
+				self._questions.append(int(retVal))
+			i++
 	def generateResponses(self):
 		self._responsePool.generatePool()
 
-	def storeResponses(self):
-		self._responsePool.storePool()
-
-	def storeResponse(self, singularResponse):
-		#TBD
-
-	def storeQuestion(self, singularQuestion):
-		#TBD
-
 	def resetSurvey(self):
-		self._questionPool.clearPool()
+		writer = SQLWriter()
+		query = "DELETE * FROM Questions"
+		dbstring = self._coursename + ".db"
+		writer.dbinsert(query, dbstring)
+		self._questionList = []
 		self._responsePool.clearPool()
 
 class Question(Object):
@@ -205,7 +241,7 @@ class Question(Object):
 	def getQuestionString():
 		return self._question
 
-	def getAswerType():
+	def getAnswerType():
 		return self._answerType
 
 	def getIsMandatory():
@@ -227,7 +263,7 @@ class Response(Object):
 
 #FOR TOMORROW
 class SQLWriter(Object):
-	def _dbselect(self, query, dbName):
+	def dbselect(self, query, dbName):
 		connection = sqlite3.connect(dbName)
 		cursorObj = connection.cursor() #Basically our file handle lol
 		rows = cursorObj.execute(query) #Add the query to the command queue, get response
@@ -238,7 +274,7 @@ class SQLWriter(Object):
 		cursorObj.close() #Close cursor (like fclose)
 		return results #We're done
 
-	def _dbinsert(self, query, dbName):
+	def dbinsert(self, query, dbName):
 		connection = sqlite3.conect(dbName)
 		cursorObj = connection.cursor()
 		cursorObj.execute(query)
